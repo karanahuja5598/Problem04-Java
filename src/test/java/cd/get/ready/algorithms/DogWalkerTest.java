eb85348b5bc2a65916f562de14afe2c46104de29
kburd <kalebmburd@gmail.com>
2020-09-22 19:21:02 -0400

    tweaks

--- src/test/java/cd/get/ready/algorithms/DogWalkerTest.java
+++ src/test/java/cd/get/ready/algorithms/DogWalkerTest.java
@@ -1,9 +1,11 @@
 package cd.get.ready.algorithms;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 class DogWalkerTest {
 
+    @Test
     public void testOne(){
         DogWalker dw = new DogWalker();
         int[] dogs = {1, 1, 3, 1, 2, 1, 3, 3, 3, 3};
@@ -12,6 +14,7 @@ class DogWalkerTest {
         Assert.assertEquals(expected, actual);
     }
 
+    @Test
     public void testTwo(){
         DogWalker dw = new DogWalker();
         int[] dogs = {10, 20, 10, 10, 30, 50, 10, 20, 30};
