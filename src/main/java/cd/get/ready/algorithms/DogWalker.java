040f9a7ee0506ce140d2cb42a309bd094e99dcb6
Karan Ahuja <karanahuja5598@gmail.com>
2020-09-30 10:04:32 -0500

    Fixed code from feedback

--- src/main/java/cd/get/ready/algorithms/DogWalker.java
+++ src/main/java/cd/get/ready/algorithms/DogWalker.java
@@ -8,8 +8,6 @@ public class DogWalker {
     public int numberOfWalksCalculator(int[] dogSizes){
         int numberOfWalks = 0;
 
-        Arrays.sort(dogSizes);
-
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 
         for (int i = 0; i < dogSizes.length; i++) {
@@ -17,19 +15,14 @@ public class DogWalker {
         }
 
         for (Integer i : map.values())
-            if (i % 2 ==0) {
+            if(i % 2 == 0) {
                 int result = i / 2;
                 numberOfWalks += result;
-            } else {
-                if(i == 1) {
-                    numberOfWalks++;
-                } else if(i % 2 != 0) {
-                    int result = i - 1;
-                    if(result % 2 == 0) {
-                        int result1 = result / 2;
-                        numberOfWalks += result1 + 1;
-                    }
-                }
+            } else if (i == 1) {
+                numberOfWalks++;
+            } else if(i % 2 != 0 && (i - 1) % 2 == 0) {
+                int result1 = i / 2;
+                numberOfWalks += result1 + 1;
             }
 
         return numberOfWalks;
